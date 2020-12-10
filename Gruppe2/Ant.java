51bc81973264a0869449c99e32b6b4a82fa61668
jomerlino <57487488+jomerlino@users.noreply.github.com>
2020-12-10 12:28:11 +0100

    Ant dazu Gruppe 2

---
+++ Gruppe2/Ant.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot und MouseInfo)
+
+/**
+ * Ergänzen Sie hier eine Beschreibung für die Klasse Ant.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Ant extends Actor
+{
+    /**
+     * Act - tut, was auch immer Ant tun will. Diese Methode wird aufgerufen, 
+     * sobald der 'Act' oder 'Run' Button in der Umgebung angeklickt werden. 
+     */
+    public void act() 
+    {
+        // Ergänzen Sie Ihren Quelltext hier...
+    }    
+}
